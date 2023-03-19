bec3308a841c9219fd5b7dc6cfcea84a0bd95711
Jouni Kantola <jouni.kantola@nepa.com>
2023-03-19 23:15:44 +0100

    Option to signal if member is applicable

--- resources/js/team.js
+++ resources/js/team.js
@@ -2,15 +2,16 @@ export function createTeam(members) {
     return members.map((name, i) => ({
         index: i + 1,
         name,
+        isHere: true,
     }));
 }
 
 export function generateMemberMarkup(team) {
     return team
         .map(
-            ({ name, index }) =>
+            ({ name, index, isHere }) =>
                 `<div class="grid user" data-index="${index}">
-<input type="checkbox" role="switch" checked />
+<input type="checkbox" role="switch" ${isHere && "checked"} />
 <input type="text" placeholder="Name" value="${name}" data-mob-user="${name}" />
 </div>`
         )
