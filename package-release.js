0c748062e1844d0d3f95cfd268b53351766aff35
Jouni Kantola <jouni.kantola@nepa.com>
2023-06-16 01:55:19 +0200

    Bundle required WebView2Loader.dll in release

--- package-release.js
+++ package-release.js
@@ -1,5 +1,6 @@
 import AdmZip from "adm-zip";
 import rcedit from "rcedit";
+import cpy from "cpy";
 
 const neuZip = new AdmZip("dist/mobtimer-release.zip");
 neuZip.extractAllTo("dist/release", true);
@@ -7,6 +8,8 @@ neuZip.extractAllTo("dist/release", true);
 await rcedit("dist/release/mobtimer-win_x64.exe", {
     icon: "resources/icons/appIcon.ico",
 });
+
+await cpy("lib/*", "dist/release");
 
 const releaseZip = new AdmZip();
 releaseZip.addLocalFolder("dist/release");
