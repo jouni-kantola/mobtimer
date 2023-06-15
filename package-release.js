3a5109f0fe081f898399940cc1a58869d2b07166
Jouni Kantola <jouni.kantola@nepa.com>
2023-06-15 23:05:16 +0200

    Include icon resource for Windows executable

---
+++ package-release.js
@@ -0,0 +1,13 @@
+import AdmZip from "adm-zip";
+import rcedit from "rcedit";
+
+const neuZip = new AdmZip("dist/mobtimer-release.zip");
+neuZip.extractAllTo("dist/release", true);
+
+await rcedit("dist/release/mobtimer-win_x64.exe", {
+    icon: "resources/icons/appIcon.ico",
+});
+
+const releaseZip = new AdmZip();
+releaseZip.addLocalFolder("dist/release");
+releaseZip.writeZip("dist/mobtimer-release.zip");
