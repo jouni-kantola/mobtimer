e1fea179b6ca1de9d1f1b31dcacb03229741202f
Jouni Kantola <jouni@kantola.se>
2023-07-06 13:29:00 +0200

    Format file

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,15 @@
-import { defineConfig } from "vite"
-import vue from "@vitejs/plugin-vue"
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  root: "resources",
-  build: {
-    outDir: "../dist",
-    emptyOutDir: true,
-  },
-  server: {
-    port: 3000
-  },
-  plugins: [vue()],
-})
+    root: "resources",
+    build: {
+        outDir: "../dist",
+        emptyOutDir: true,
+    },
+    server: {
+        port: 3000,
+    },
+    plugins: [vue()],
+});
