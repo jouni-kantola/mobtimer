0073a849a51ff119a116a9f51eaa386fc0185f95
Jouni Kantola <jouni@kantola.se>
2023-07-27 23:07:29 +0200

    Type emitted events

--- resources/js/components/IntervalLength.vue
+++ resources/js/components/IntervalLength.vue
@@ -5,16 +5,19 @@
 <script setup lang="ts">
 import { ref } from "vue";
 
-const emit = defineEmits(["intervalUpdated", "enterKeyDown"]);
+const emit = defineEmits<{
+    intervalUpdated: [number]
+    enterKeyDown: []
+}>()
 
 const intervalLength = ref(600);
 
 function onInput(event: Event) {
     intervalLength.value = Math.max(1, parseInt((event.target as HTMLInputElement).value) || 1);
-    emit('intervalUpdated', intervalLength.value)
+    emit("intervalUpdated", intervalLength.value)
 }
 
 function onKeydown(event: KeyboardEvent) {
-    if (event.key === "Enter") emit('enterKeyDown');
+    if (event.key === "Enter") emit("enterKeyDown");
 }
 </script>
