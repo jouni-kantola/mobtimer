7726f8c6e9dd37bed9abcffc948f8a6ddd392fdb
Jouni Kantola <jouni@kantola.se>
2023-07-27 12:03:23 +0200

    Define type for team member

--- resources/js/team.ts
+++ resources/js/team.ts
@@ -1,4 +1,11 @@
-export function createTeam(members) {
+type Member = {
+    index: number;
+    name: string;
+    isHere: boolean;
+    isActive: boolean;
+};
+
+export function createTeam(members: Array<string>): Array<Member> {
     return members.map((name, index) => ({
         index,
         name,
@@ -7,7 +14,7 @@ export function createTeam(members) {
     }));
 }
 
-export function whosNextAfter(recentActiveIndex, team) {
+export function whosNextAfter(recentActiveIndex: number, team: Array<Member>) {
     const presentMembers = team.filter(m => m.isHere);
     if (presentMembers.length === 1) return presentMembers[0];
 
@@ -19,28 +26,32 @@ export function whosNextAfter(recentActiveIndex, team) {
         );
 
         const upcomingMember = getMemberByIndex(nextMemberIndex, team);
-        if (upcomingMember.isHere) return upcomingMember;
+        if (upcomingMember?.isHere) return upcomingMember;
     }
 }
 
-function getMemberByIndex(index, team) {
+function getMemberByIndex(index: number, team: Array<Member>) {
     return team.find(m => m.index === index);
 }
 
-export function switchActiveMember(nextIndex, team) {
+export function switchActiveMember(nextIndex: number, team: Array<Member>) {
     for (const member of team) {
         member.isActive = member.index === nextIndex;
     }
 }
 
-export function getActiveMember(team) {
+export function getActiveMember(team: Array<Member>) {
     return team.find(m => m.isActive);
 }
 
-export function getLast(team) {
+export function getLast(team: Array<Member>) {
     return team.filter(m => m.isHere).slice(-1)[0];
 }
 
-function getNextMemberIndex(recentActiveIndex, nextIndex, teamSize) {
+function getNextMemberIndex(
+    recentActiveIndex: number,
+    nextIndex: number,
+    teamSize: number
+) {
     return (recentActiveIndex + nextIndex) % teamSize;
 }
