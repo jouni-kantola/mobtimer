91546cca5129f3c6ea766a1b41737c1a645781cd
Jouni Kantola <jouni@kantola.se>
2023-07-29 18:10:24 +0200

    Pass expected function args

--- test/clock-test.ts
+++ test/clock-test.ts
@@ -3,13 +3,13 @@ import { startTimer } from "../resources/js/clock";
 
 test("determine if running", t => {
     const seconds = 1;
-    const timer = startTimer(seconds);
+    const timer = startTimer(seconds, () => {}, () => {});
     assert.ok(timer.isRunning);
 });
 
 test("stopped when interval ends", async t => {
     const seconds = 1;
-    const timer = startTimer(seconds);
+    const timer = startTimer(seconds, () => {}, () => {});
 
     assert.ok(timer.isRunning);
     await new Promise((resolve, _) => {
@@ -22,25 +22,25 @@ test("callbacks every tick", async t => {
     const seconds = 3;
     let timesCalled = 0;
 
-    await new Promise((resolve, _) => {
+    await new Promise<void>((resolve, _) => {
         startTimer(seconds, () => {
             timesCalled++;
             if (timesCalled === 2) {
                 resolve();
             }
-        });
+        }, () => {});
     });
 });
 
 test("notify when countdown done", async t => {
-    await new Promise((resolve, _) => {
-        return startTimer(1, undefined, resolve);
+    await new Promise<void>((resolve, _) => {
+        return startTimer(1, () => {}, resolve);
     });
 });
 
 test("provide formatted time left", async t => {
     const seconds = 600;
-    const timer = startTimer(seconds);
+    const timer = startTimer(seconds, () => {}, () => {});
 
     await new Promise((resolve, _) => {
         setTimeout(resolve, 1500);
@@ -53,7 +53,7 @@ test("provide formatted time left", async t => {
 });
 
 test("can change timer", async t => {
-    const timer = startTimer(600);
+    const timer = startTimer(600, () => {}, () => {});
     timer.change(300);
     assert.strictEqual(timer.timeLeft, "05:00");
     timer.change(1);
@@ -61,7 +61,7 @@ test("can change timer", async t => {
 });
 
 test("can reset started timer", async t => {
-    const timer = startTimer(600);
+    const timer = startTimer(600, () => {}, () => {});
 
     assert.ok(timer.isRunning);
 
@@ -76,7 +76,7 @@ test("can reset started timer", async t => {
 });
 
 test("can pause timer", async t => {
-    const timer = startTimer(600);
+    const timer = startTimer(600, () => {}, () => {});
 
     assert.ok(timer.isRunning);
 
