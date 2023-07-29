595d3baf082fb7f97309f4603485e479cd43f1cc
Jouni Kantola <jouni@kantola.se>
2023-07-29 18:48:49 +0200

    Introduce Member component

---
+++ resources/js/components/Member.vue
@@ -0,0 +1,14 @@
+<template>
+    <input type="checkbox" v-model="isHere" @change="toggleMemberHere">
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const isHere = ref(true);
+
+function toggleMemberHere(event: Event) {
+    const isHereToggle = event.target as HTMLInputElement;
+    isHere.value = isHereToggle.checked;
+}
+</script>
