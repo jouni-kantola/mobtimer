debaf4daa31e656d8cb3b4d3a37815d7d1ee87b3
Jouni Kantola <jouni.kantola@nepa.com>
2023-07-29 13:59:07 +0200

    Setup Vue unit testing

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from "vitest/config";
+import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
     test: {
         include: ["test/**/*-test.[jt]s"],
+        environment: "happy-dom",
     },
+    plugins: [vue()],
 });
