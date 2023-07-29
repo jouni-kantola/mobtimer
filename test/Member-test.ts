b106c81d26376078fb68ba1ae96c31ae2037c9a3
Jouni Kantola <jouni@kantola.se>
2023-07-29 18:48:49 +0200

    Introduce Member component

---
+++ test/Member-test.ts
@@ -0,0 +1,13 @@
+import { assert, test } from "vitest";
+import { mount } from "@vue/test-utils";
+import Member from "../resources/js/components/Member.vue";
+
+test("here status can be toggled", async () => {
+    const wrapper = mount(Member);
+
+    const isHereToggle = wrapper.find("input[type='checkbox']:checked");
+    
+    await isHereToggle.trigger("click");
+    
+    assert.equal((isHereToggle.element as HTMLInputElement).checked, false);
+});
