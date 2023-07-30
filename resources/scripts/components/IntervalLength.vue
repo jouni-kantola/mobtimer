3c0a55a0ef08daf60c15d763695fb56e8893a70e
Jouni Kantola <jouni.kantola@nepa.com>
2023-07-30 23:29:59 +0200

    Modified text input to enable padding with zeroes

--- resources/scripts/components/IntervalLength.vue
+++ resources/scripts/components/IntervalLength.vue
@@ -1,23 +1,52 @@
 <template>
-    <input type="number" v-model="intervalLength" @input="onInput" @keydown="onKeydown" />
+    <input
+        type="text"
+        :value="paddedValue"
+        @input="onInput"
+        @keydown="onKeyDown"
+    />
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
+import { computed } from "vue";
+
+const props = defineProps({
+    value: {
+        type: Number,
+        required: true,
+    },
+    min: {
+        type: Number,
+        default: 0,
+    },
+    max: {
+        type: Number,
+        default: Infinity,
+    },
+});
+
+const paddedValue = computed(() => {
+    const val = props.value > props.min ? props.value : props.min;
+    return String(Math.min(props.max, val)).padStart(2, "0");
+});
 
 const emit = defineEmits<{
-    intervalUpdated: [number]
-    enterKeyDown: []
-}>()
-
-const intervalLength = ref(600);
+    intervalUpdated: [number];
+    enterKeyDown: [];
+}>();
 
 function onInput(event: Event) {
-    intervalLength.value = Math.max(1, parseInt((event.target as HTMLInputElement).value) || 1);
-    emit("intervalUpdated", intervalLength.value)
+    const intervalLength = Math.min(
+        props.max,
+        Math.max(
+            props.min,
+            parseInt((event.target as HTMLInputElement).value) || props.min
+        )
+    );
+    emit("intervalUpdated", intervalLength);
 }
 
-function onKeydown(event: KeyboardEvent) {
+function onKeyDown(event: KeyboardEvent) {
     if (event.key === "Enter") emit("enterKeyDown");
 }
 </script>
