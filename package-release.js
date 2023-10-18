aa0e7112084394da357f44fda3dbca2df6fafad1
Jouni Kantola <jouni.kantola@nepa.com>
2023-10-18 23:47:14 +0200

    Remove noop copy libs step

--- package-release.js
+++ package-release.js
@@ -1,6 +1,5 @@
 import AdmZip from "adm-zip";
 import rcedit from "rcedit";
-import cpy from "cpy";
 
 const neuZip = new AdmZip("dist/mobtimer-release.zip");
 neuZip.extractAllTo("dist/release", true);
@@ -8,8 +7,6 @@ neuZip.extractAllTo("dist/release", true);
 await rcedit("dist/release/mobtimer-win_x64.exe", {
     icon: "resources/icons/appIcon.ico",
 });
-
-await cpy("lib/*", "dist/release");
 
 const releaseZip = new AdmZip();
 releaseZip.addLocalFolder("dist/release");
