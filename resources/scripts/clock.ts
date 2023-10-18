df18467b7e0fd926f9552a8a218b8bc04ceb03de
Jouni Kantola <jouni.kantola@nepa.com>
2023-10-18 22:04:16 +0200

    Reactive compatible timer object

--- resources/scripts/clock.ts
+++ resources/scripts/clock.ts
@@ -1,60 +1,68 @@
 export type TimeRemaining = [number, number];
 
 class Timer {
-    #intervalSeconds: number;
-    #secondsLeft: number;
-    #onTick: (timeLeft: TimeRemaining) => void;
-    #onEnd: () => void;
-    #clockIntervalId: NodeJS.Timeout | null;
+    private intervalSeconds: number;
+    private secondsLeft: number;
+    private onTick: (timeLeft: TimeRemaining) => void;
+    private onEnd: () => void;
+    private clockIntervalId: NodeJS.Timeout | null;
 
-    constructor(seconds: number, onTick: (timeLeft: TimeRemaining) => void, onEnd: () => void) {
-        this.#intervalSeconds = seconds;
-        this.#secondsLeft = seconds;
-        this.#onTick = onTick;
-        this.#onEnd = onEnd;
-        this.#clockIntervalId = null;
+    constructor(
+        seconds: number,
+        onTick: (timeLeft: TimeRemaining) => void,
+        onEnd: () => void
+    ) {
+        this.intervalSeconds = seconds;
+        this.secondsLeft = seconds;
+        this.onTick = onTick;
+        this.onEnd = onEnd;
+        this.clockIntervalId = null;
     }
 
     get isRunning() {
-        return !!this.#clockIntervalId;
+        return !!this.clockIntervalId;
     }
 
     get timeLeft() {
-        return secondsToMinutesAndSeconds(this.#secondsLeft);
+        return secondsToMinutesAndSeconds(this.secondsLeft);
     }
 
     start() {
-        this.#clockIntervalId = setInterval(() => {
-            if (--this.#secondsLeft === 0) {
-                this.#clearInterval();
-                !!this.#onEnd && this.#onEnd();
+        this.clockIntervalId = setInterval(() => {
+            if (--this.secondsLeft === 0) {
+                this.clearInterval();
+                !!this.onEnd && this.onEnd();
             } else {
-                !!this.#onTick && this.#onTick(this.timeLeft);
+                !!this.onTick && this.onTick(this.timeLeft);
             }
         }, 1000);
     }
 
     change(seconds: number) {
-        this.#intervalSeconds = seconds;
-        this.#secondsLeft = seconds;
+        this.intervalSeconds = seconds;
+        this.secondsLeft = seconds;
     }
 
     reset() {
-        this.#clearInterval();
-        this.change(this.#intervalSeconds);
+        this.clearInterval();
+        this.change(this.intervalSeconds);
     }
 
     pause() {
-        this.#clearInterval();
+        this.clearInterval();
     }
 
-    #clearInterval() {
-        !!this.#clockIntervalId && clearInterval(this.#clockIntervalId);
-        this.#clockIntervalId = null;
+    clearInterval() {
+        !!this.clockIntervalId && clearInterval(this.clockIntervalId);
+        this.clockIntervalId = null;
     }
 }
 
-export function startTimer(seconds: number, onTick: (timeLeft: TimeRemaining) => void, onEnd: () => void) {
+export function startTimer(
+    seconds: number,
+    onTick: (timeLeft: TimeRemaining) => void,
+    onEnd: () => void
+) {
     const timer = new Timer(seconds, onTick, onEnd);
     timer.start();
     return timer;
