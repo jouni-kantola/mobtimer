2856185eac9926d92f88adb009d58ca4387280c2
Jouni Kantola <jouni.kantola@nepa.com>
2024-11-21 23:18:13 +0100

    Fix serving __neutralino_globals.js

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,41 @@
+import type { Plugin, ResolvedConfig } from "vite";
+
 import { defineConfig } from "vite";
+import fs from "node:fs/promises";
+import path from "node:path";
+
 import vue from "@vitejs/plugin-vue";
+
+const neutralino = (): Plugin => {
+    let config: ResolvedConfig;
+
+    return {
+        name: "neutralino",
+        configResolved(resolvedConfig) {
+            config = resolvedConfig;
+        },
+        async transformIndexHtml(html) {
+            if (config.mode === "development") {
+                const authInfoFile = await fs.readFile(
+                    path.join(".tmp", "auth_info.json"),
+                    {
+                        encoding: "utf-8",
+                    }
+                );
+
+                const authInfo = JSON.parse(authInfoFile);
+                const port = authInfo.nlPort;
+
+                return html.replace(
+                    /<script src="__neutralino_globals\.js"><\/script>/,
+                    `<script src="http://localhost:${port}/__neutralino_globals.js"></script>`
+                );
+            }
+
+            return html;
+        },
+    };
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,5 +47,5 @@ export default defineConfig({
     server: {
         port: 3000,
     },
-    plugins: [vue()],
+    plugins: [vue(), neutralino()],
 });
